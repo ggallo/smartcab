02a255832cfaed110a05acd8cdafcf5dad00924e
George Gallo <georgeggallo@gmail.com>
2017-01-18 13:42:54 -0500

    feat: Create optimized agent
    Use linear epsilon decay of 0.01 per trial.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -19,10 +19,8 @@ class LearningAgent(Agent):
         self.epsilon = epsilon   # Random exploration factor
         self.alpha = alpha       # Learning factor
 
-        ###########
-        ## TO DO ##
-        ###########
         # Set any additional class parameters as needed
+        self.trialNumber = 0
 
 
     def reset(self, destination=None, testing=False):
@@ -30,13 +28,18 @@ class LearningAgent(Agent):
             'testing' is set to True if testing trials are being used
             once training trials have completed. """
 
+        # Increase trial counter
+        self.trialNumber += 1
         # Select the destination as the new location to route to
         self.planner.route_to(destination)
 
-
         # Update epsilon using a decay function of your choice
-        self.epsilon -= 0.05
-        # Update additional class parameters as needed
+        self.epsilon -= 0.01
+        # Update alpha too
+        # self.alpha -= 0.005
+        # if self.alpha < 0:
+        #     # But don't allow alpha to go below 0
+        #     self.alpha = 0
 
         # If 'testing' is True, set epsilon and alpha to 0
         if testing:
@@ -54,7 +57,7 @@ class LearningAgent(Agent):
         inputs = self.env.sense(self)           # Visual input - intersection light and traffic
         deadline = self.env.get_deadline(self)  # Remaining deadline
 
-        state = (waypoint, inputs['light'], inputs['left'], inputs['right'], inputs['oncoming'])
+        state = (waypoint, inputs['light'], inputs['oncoming'])
 
         return state
 
@@ -154,7 +157,7 @@ def run():
     #   learning   - set to True to force the driving agent to use Q-learning
     #    * epsilon - continuous value for the exploration factor, default is 1
     #    * alpha   - continuous value for the learning rate, default is 0.5
-    agent = env.create_agent(LearningAgent, learning=True)
+    agent = env.create_agent(LearningAgent, learning=True, alpha=0.5)
 
     ##############
     # Follow the driving agent
@@ -169,14 +172,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay=0.01, display=False, log_metrics=True)
+    sim = Simulator(env, update_delay=0.005, display=False, log_metrics=True, optimized=True)
 
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10)
+    sim.run(tolerance=0.05, n_test=30)
 
 
 if __name__ == '__main__':
